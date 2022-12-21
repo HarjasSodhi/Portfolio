7e7fb2f3885f4e5c9edbda17f85268b0bfec089f
HarjasSodhi <harjassodhi@yahoo.in>
2022-12-22 02:41:37 +0530

    socials bars added

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -40,7 +40,7 @@ function Nav() {
     }, [scroll, vis]);
 
     return (
-        <div ref={navRef} className='transform translate-y-none transition ease duration-500 sticky w-full top-0 left-0 z-10 flex justify-between items-center bg-[#0a192f] text-white text-sm shadow-black'>
+        <div ref={navRef} className='transform translate-y-none transition ease duration-500 sticky w-full top-0 left-0 z-10 flex justify-between items-center bg-[#0a192f] text-white text-sm shadow-black p-4'>
             <div className="logo m-4 text-lg text-[#64ffda] font-bold">HarjasSodhi</div>
             <div className="absolute text-[#64ffda] z-30 top-0 right-0 nav-toggle md:hidden m-5 text-2xl" onClick={handleVis}><i className={`${vis ? "fa-sharp fa-solid fa-xmark" : "fa-solid fa-bars"}`}></i></div>
             <div className={`fixed flex text-xl md:text-sm flex-col justify-center items-center top-0 right-0 z-20 h-screen w-2/4 md:h-fit md:w-fit bg-[#112240] md:bg-inherit md:static md:flex-row links md:items-center transform ${vis ? 'translate-x-none' : 'translate-x-full'} md:transform-none transition ease 0.5s opacity-90 shadow-2xl md:shadow-none shadow-black`}>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadein: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+    },
   },
   plugins: [],
 }
