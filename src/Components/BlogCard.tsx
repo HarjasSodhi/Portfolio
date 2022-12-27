d7001dc301327e18459886ca377315c08303b869
HarjasSodhi <harjassodhi@yahoo.in>
2022-12-27 20:58:50 +0530

    scroll bar

--- src/Components/BlogCard.tsx
+++ src/Components/BlogCard.tsx
@@ -6,9 +6,9 @@ type propType = {
 
 function BlogCard(props: propType) {
     return (
-        <Link className="transition-all hover:-translate-y-1 hover:border-[#64ffda] tracking-wider hover:text-[#64ffda] border-white bg-[#112240] opacity-90 p-5 md:text-xl text-lg text-[#ccd6f6] font-bold rounded-xl shadow-md shadow-black flex gap-4" to={"/blog/" + props.el.split(' ').join("_")}>
+        <Link className="duration-200 blog-card transition-all transform hover:-translate-y-2 hover:border-[#64ffda] tracking-wider hover:text-[#64ffda] border-white bg-[#112240] opacity-90 p-5 md:text-xl text-lg text-[#ccd6f6] font-bold rounded-xl shadow-md shadow-black flex gap-4" to={"/blog/" + props.el.split(' ').join("_")}>
             <span><i className="fa-solid fa-person-chalkboard"></i></span>
-            <span>{props.el}</span>
+            <span className='blog-title'>{props.el}</span>
         </Link>
     )
 }
